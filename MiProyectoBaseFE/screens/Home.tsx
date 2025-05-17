0d084fe48586900fcc22bccf2bdc05b28fa8e8ef
Maria Jose Salinas <mariasalinas713@gmail.com>
2025-05-16 19:22:31 -0600

    agregando estilos y estructura a home y login

--- MiProyectoBaseFE/screens/Home.tsx
+++ MiProyectoBaseFE/screens/Home.tsx
@@ -1,13 +1,22 @@
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import CustomButton from '../components/CustomButton';
 
 export default function Home({ navigation, route }: any) {
-const {email, password } = route.params;
-//const {password} = route.params;
+  const { email } = route.params;
 
-    return(
-        <View>
-            <Text>Bienvenido: {email}</Text>
-            </View>
-
-    )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bienvenido, {email}</Text>
+     
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, 
+    justifyContent: 'flex-start', 
+    alignItems: 'center', 
+    paddingTop: 20 },
+  title: { fontSize: 20, marginBottom: 20 },
+});
--- MiProyectoBaseFE/screens/Login.tsx
+++ MiProyectoBaseFE/screens/Login.tsx
@@ -1,34 +1,75 @@
 import { MaterialIcons } from "@expo/vector-icons";
-import { Button, TouchableOpacity, View, Text } from "react-native";
+import { Button, TouchableOpacity, View, Text, StyleSheet } from "react-native";
 import CustomInput from "../components/CustomInput";
 import React, { useState } from "react";
-
+import CustomButton from "../components/CustomButton";
 
 export default function Login({ navigation }: any) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+
     return (
-        <View>
-            <CustomInput
-                label="Correo"
-                type="email"
-                value={email}
-                onChange={setEmail}
-                required={false} />
+        <View style={styles.container}>
+            <View style={styles.backgroundCard}>
 
-            <CustomInput
-                label="Contraseña"
-                type="password"
-                value={password}
-                onChange={setPassword}
-                required={true} />
+                <Text style={styles.title}>Iniciar Sesión</Text>
+                <CustomInput
+                    label="Correo"
+                    type="email"
+                    value={email}
+                    onChange={setEmail}
+                    required={false} />
 
-            <TouchableOpacity onPress={() =>{navigation.navigate('HomeScreen', 
-                { email, password })}}>
-                <Text>Ingresar</Text>
-            </TouchableOpacity>
+                <CustomInput
+                    label="Contraseña"
+                    type="password"
+                    value={password}
+                    onChange={setPassword}
+                    required={true} />
+
+
+                <CustomButton
+                    title="Ingresar"
+                    onPress={() => {
+                        navigation.navigate('HomeScreen',
+                            { email, password })
+                    }}
+                    variant="primary" />
+
+                <CustomButton
+                    title="Registrarse"
+                    onPress={() => {
+                        navigation.navigate('HomeScreen',
+                            { email, password })
+                    }}
+                    variant="secondary" />
+            </View>
         </View>
 
     )
 }
+
+const styles = StyleSheet.create({
+    title: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 24,
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#161640',
+    },
+    backgroundCard: {
+        backgroundColor: 'lightblue',
+        borderRadius: 10,
+        padding: 40,
+        paddingTop: 60,
+        paddingBottom: 60,
+        width: '70%',
+        height: '80%',
+    }
+
+})
